ab251489ae8c4f72965eab5f8bf19a9f9e118645
abbymanalo <abrielle.manalo.08@gmail.com>
2024-12-11 10:32:27 -0800

    call student data into app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify 
 from flask_cors import CORS
 
 # initialize a flask application (app)
@@ -12,63 +12,27 @@ CORS(app, supports_credentials=True, origins='*')  # Allow all origins (*)
 def get_data():
     # start a list, to be used like a information database
     InfoDb = []
-    # add a row to list, an Info record
-    InfoDb.append({
-            "FirstName": "Ava",
-            "LastName": "Shalon",
-            "Favorite_Color": "blue",
-            "Favorite_Fruit": "Avocado",
-            "Hobbies": ["Watching TV", "cooking", "swimming", "going to the beach"]
-            })
 
     # add a row to list, an Info record
     InfoDb.append({
-            "FirstName": "Elliot",
-            "LastName": "Yang",
-            "Favorite_Color": "purple",
-            "Favorite_Fruit": "watermelon",
-            "Favorite Sport": "jmortensen@powayusd.com",
-            "Hobbies": ["reading", "exercising", "going to the beach", "hiking", "singing", "playing instruments"]
-            })  
-    
-    InfoDb.append({
-            "FirstName": "Risha",
-            "LastName": "Guha",
-            "FavoriteColor": "Blue",
-            "FavoriteFruit": "mango",
-            "FavoriteSport": "badminton", 
-            "Hobbies": ["Reading", "Piano", "Cybersecurity", "Video Games", "Debate"]
-            })
-    
-    InfoDb.append({
-            "FirstName": "Shriya",
-            "LastName": "Paladugu",
-            "FavoriteColor": "Pink",
-            "FavoriteFruit": "Orange",
-            "FavoriteSport": "Basketball",
-            "Hobbies": ["Basketball", "CyberSecurity", "Hanging out with friends", "", "Speech and Debate"]
-            })
-    
-    InfoDb.append({
-            "FirstName": "Abby",
-            "LastName": "Manalo",
-            "FavoriteColor": "Purple",
-            "FavoriteFruit": "Mango",
-            "FavoriteSport": "Dance", 
-            "Hobbies": ["Baking", "Watching Shows", "Reading", "Coloring",]
-            })
-    
-    InfoDb.append({
-            "FirstName": "Aranya",
-            "LastName": "Bhattacharya",
-            "FavoriteColor": "Orange",
-            "FavoriteFruit": "Mango",
-            "FavoriteSport": "Boxing", 
-            "Hobbies": ["Watching Movies", "Walking Dog", "Machine Learning", "Excercise",]
-            })
-    
-    
+        "FirstName": "John",
+        "LastName": "Mortensen",
+        "DOB": "October 21",
+        "Residence": "San Diego",
+        "Email": "jmortensen@powayusd.com",
+        "Owns_Cars": ["2015-Fusion", "2011-Ranger", "2003-Excursion", "1997-F350", "1969-Cadillac"]
+    })
 
+    # add a row to list, an Info record
+    InfoDb.append({
+        "FirstName": "Shane",
+        "LastName": "Lopez",
+        "DOB": "February 27",
+        "Residence": "San Diego",
+        "Email": "slopez@powayusd.com",
+        "Owns_Cars": ["2021-Insight"]
+    })
+    
     return jsonify(InfoDb)
 
 # add an HTML endpoint to flask app
@@ -77,7 +41,7 @@ def say_hello():
     html_content = """
     <html>
     <head>
-        <title>Hellox</title>
+        <title>Hello</title>
     </head>
     <body>
         <h2>Hello, World!</h2>
@@ -85,6 +49,87 @@ def say_hello():
     </html>
     """
     return html_content
+
+
+@app.route('/student')
+def print_data_student():
+    html_content = """
+    
+<h1>Access data from our Flask server using JavaScript</h1>
+
+<p>This code extracts data "live" from a local Web Server with JavaScript fetch.  Additionally, it formats the data into a table.</p>
+
+<!-- Head contains information to Support the Document -->
+
+
+<!-- HTML table fragment for page -->
+<table id="demo" class="table">
+  <thead>
+      <tr>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Residence</th>
+      </tr>
+  </thead>
+  <tbody id="result">
+    <!-- javascript generated data -->
+  </tbody>
+</table>
+
+<script>
+  // prepare HTML result container for new output
+  let resultContainer = document.getElementById("result");
+  
+  // prepare URL
+  url = "http://127.0.0.1:5001/api/data";
+
+  // set options for cross origin header request
+  let options = {
+    method: 'GET', // *GET, POST, PUT, DELETE, etc.
+    mode: 'cors', // no-cors, *cors, same-origin
+    cache: 'default', // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: 'include', // include, *same-origin, omit
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+
+  // fetch the API
+  fetch(url, options)
+    // response is a RESTful "promise" on any successful fetch
+    .then(response => {
+      // check for response errors and display
+      if (response.status !== 200) {
+          console.error(response.status);
+          return;
+      }
+      // valid response will contain json data
+      response.json().then(data => {
+          console.log(data);
+          for (const row of data) {
+            // tr and td build out for each row
+            const tr = document.createElement("tr");
+            const firstname = document.createElement("td");
+            const lastname = document.createElement("td");
+            const residence = document.createElement("td");
+            // data is specific to the API
+            firstname.innerHTML = row.FirstName; 
+            lastname.innerHTML = row.LastName; 
+            residence.innerHTML = row.Residence; 
+            // this builds each td into tr
+            tr.appendChild(firstname);
+            tr.appendChild(lastname);
+            tr.appendChild(residence);
+            // add HTML to container
+            resultContainer.appendChild(tr);
+          }
+      })
+  })
+  
+</script>
+    
+    """
+    return html_content 
 
 if __name__ == '__main__':
     # starts flask server on default port, http://127.0.0.1:5001
