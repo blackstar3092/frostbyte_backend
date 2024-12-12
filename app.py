97ec8f090d62e179bed63ec931902c44c65a3ffb
abbymanalo <abrielle.manalo.08@gmail.com>
2024-12-12 09:06:58 -0800

    typos in app data

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ def get_data():
         "LastName": "Yang",
         "FavoriteColor": "purple",
         "FavoriteFruit": "watermelon",
-        "FavoriteSport": "jmortensen@powayusd.com",
+        "FavoriteSport": "none",
         "Hobbies": ["Reading",  "Exercising",  "Going to the beach",  "Hiking",  "Singing",  "Playing instruments"]
     })
     
