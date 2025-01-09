7e96e0bd745584d142933e3a9eddfd8cb8b6b65e
ava-dn <ashalon@chasd.org>
2025-01-08 17:37:48 -0800

    map backend

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from app import app  # Import your Flask app from app.py
-from location_routes import location_bp  # Import the location blueprint from location_routes.py
+from model.location_routes import location_bp  # Import the location blueprint from location_routes.py
 
 # Register the blueprint in a separate script
 app.register_blueprint(location_bp)
