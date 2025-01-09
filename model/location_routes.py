0162a208734491db8cdf3690c68baf6629435150
ava-dn <ashalon@chasd.org>
2025-01-09 08:36:06 -0800

    breakpoint add

--- model/location_routes.py
+++ model/location_routes.py
@@ -1,9 +1,9 @@
 import json
 from flask import Blueprint, request, jsonify
+import pdb  # Import the pdb module for debugging
 
 location_bp = Blueprint('location', __name__)
 
-# Read the saved locations from a file on app start
 def read_locations():
     try:
         with open('locations.json', 'r') as f:
@@ -11,16 +11,15 @@ def read_locations():
     except FileNotFoundError:
         return []
 
-# Write locations to the file
 def write_locations(locations):
     with open('locations.json', 'w') as f:
         json.dump(locations, f)
 
-# Retrieve locations from the file
 locations = read_locations()
 
 @location_bp.route('/api/save-location', methods=['POST'])
 def save_location():
+    pdb.set_trace()  # Breakpoint 1
     data = request.get_json()
 
     lat = data.get('lat')
@@ -30,10 +29,12 @@ def save_location():
         return jsonify({"error": "Invalid data"}), 400
 
     locations.append({"lat": lat, "lng": lng})
-    write_locations(locations)  # Save the updated locations to the file
+    write_locations(locations)  
 
+    pdb.set_trace()  # Breakpoint 2
     return jsonify({"message": "Location saved successfully!"}), 200
 
 @location_bp.route('/api/get-locations', methods=['GET'])
 def get_locations():
+    pdb.set_trace()  # Breakpoint 3
     return jsonify({"locations": locations}), 200
