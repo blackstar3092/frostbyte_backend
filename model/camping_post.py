935f10f4062059458f5a7e44b9e2b36da7047983
abbymanalo <abrielle.manalo.08@gmail.com>
2025-01-28 02:41:46 -0800

    simplified

--- model/camping_post.py
+++ model/camping_post.py
@@ -42,15 +42,7 @@ class camping(db.Model):
         return self
         
     def read(self):
-        """
-        The read method retrieves the object data from the object's attributes and returns it as a dictionary.
         
-        Uses:
-            The Channel.query and User.query methods to retrieve the channel and user objects.
-        
-        Returns:
-            dict: A dictionary containing the post data, including user and channel names.
-        """
         user = Frostbyte.query.get(self._user_id)
         channel = Channel.query.get(self._channel_id)
         data = {
@@ -63,67 +55,8 @@ class camping(db.Model):
         return data
     
 
-    '''def update(self):
-        """
-        Updates the post object with new data.
-        
-        Args:
-            inputs (dict): A dictionary containing the new data for the post.
-        
-        Returns:
-            Post: The updated post object, or None on error.
-        """
-        
-        inputs = Post.query.get(self.id)
-        
-        title = inputs._title
-        content = inputs._content
-        channel_id = inputs._channel_id
-        user_name = Frostbyte.query.get(inputs._user_id).name if inputs._user_id else None
-        channel_name = Channel.query.get(inputs._channel_id).name if inputs._channel_id else None
-
-        # If channel_name is provided, look up the corresponding channel_id
-        if channel_name:
-            channel = Channel.query.filter_by(_name=channel_name).first()
-            if channel:
-                channel_id = channel.id
-                
-        if user_name:
-            user = Frostbyte.query.filter_by(_name=user_name).first()
-            if user:
-                user_id = user.id
-            else:
-                return None
-
-        # Update table with new data
-        if title:
-            self._title = title
-        if content:
-            self._content = content
-        if channel_id:
-            self._channel_id = channel_id
-        if user_id:
-            self._user_id = user_id
-
-        try:
-            db.session.commit()
-        except IntegrityError:
-            db.session.rollback()
-            logging.warning(f"IntegrityError: Could not update post with title '{title}' due to missing channel_id.")
-            return None
-        return self'''
-    
-
     def update(self, inputs):
-        """
-        Updates the post object with new data.
         
-        Args:
-            inputs (dict): A dictionary containing the new data for the post.
-        
-        Returns:
-            Post: The updated post object, or None on error.
-        """
         if not isinstance(inputs, dict):
             return None  # Return None if inputs are invalid
 
@@ -143,15 +76,7 @@ class camping(db.Model):
 
     
     def delete(self):
-        """
-        The delete method removes the object from the database and commits the transaction.
-        
-        Uses:
-            The db ORM methods to delete and commit the transaction.
-        
-        Raises:
-            Exception: An error occurred when deleting the object from the database.
-        """    
+           
         try:
             db.session.delete(self)
             db.session.commit()
@@ -159,36 +84,6 @@ class camping(db.Model):
             db.session.rollback()
             raise e
         
-    ''' @staticmethod
-    def restore(data):
-        for post_data in data:
-            _ = post_data.pop('id', None)  # Remove 'id' from post_data
-            title = post_data.get("title", None)
-            post = Post.query.filter_by(_title=title).first()
-            if post:
-                post.update(post_data)
-            else:
-                post = Post(**post_data)
-                post.update(post_data)
-                post.create()''' 
-    
-    ''' @staticmethod
-    def restore(data):
-        """
-        Restores posts from the given data.
-        
-        Args:
-            data (list): A list of dictionaries representing posts to restore.
-        """
-        for post_data in data:
-            post_id = post_data.pop('id', None)  # Remove 'id' from post_data if present
-            post = Post.query.filter_by(_title=post_data.get("title")).first()
-
-            if post:
-                post.update(post_data)  # Update existing post
-            else:
-                new_post = Post(**post_data)  # Create new post
-                new_post.create()'''
     
     @staticmethod
     def restore(data):
@@ -204,19 +99,7 @@ class camping(db.Model):
                 campingPost.create()
 
         
-def initCampingPosts():
-    """
-    The initPosts function creates the Post table and adds tester data to the table.
-    
-    Uses:
-        The db ORM methods to create the table.
-    
-    Instantiates:
-        Post objects with tester data.
-    
-    Raises:
-        IntegrityError: An error occurred when adding the tester data to the table.
-    """        
+def initCampingPosts(): 
     with app.app_context():
         """Create database and tables"""
         db.create_all()
