01905596242d15155c3e1de21b307037d687736b
yourGHID <youremail@gmail.com>
2025-02-24 10:04:45 -0800

    weather change

--- api/weather.py
+++ api/weather.py
@@ -1,8 +1,6 @@
 import requests
-from flask import Blueprint, request, jsonify, g
+from flask import Blueprint, request, jsonify
 from flask_restful import Api, Resource
-from datetime import datetime
-from __init__ import app
 from api.jwt_authorize import token_required
 
 # This Blueprint object is used to define APIs for the weather service.
@@ -15,15 +13,21 @@ api = Api(weather_api)
 API_KEY = '9bb9d39671474da4b83164647252402'
 BASE_URL = 'http://api.weatherapi.com/v1/current.json'
 
-# Locations dictionary for predefined places
-LOCATIONS = {
-    "grand-canyon": {"lat": 36.1069, "lon": -112.1129},
-    "denali": {"lat": 63.4348, "lon": -148.2670},
-    "redwood": {"lat": 41.2132, "lon": -124.0046},
-    "buck-reef": {"lat": 29.6074, "lon": -81.3886}  # Example coordinates
-}
+def fetch_coordinates(city):
+    """Fetch latitude and longitude of the city using the weatherapi.com geocoding feature"""
+    response = requests.get(BASE_URL, params={
+        'key': API_KEY,
+        'q': city,
+        'aqi': 'no'
+    })
+    
+    if response.status_code == 200:
+        data = response.json()
+        lat = data['location']['lat']
+        lon = data['location']['lon']
+        return lat, lon
+    return None
 
-@token_required()
 def fetch_weather(lat, lon):
     """Fetch weather information from the weatherapi.com"""
     response = requests.get(BASE_URL, params={
@@ -38,34 +42,39 @@ def fetch_weather(lat, lon):
             'temperature': data['current']['temp_c'],
             'description': data['current']['condition']['text'],
             'humidity': data['current']['humidity'],
-            'pressure': data['current']['pressure_mb']
+            'pressure': data['current']['pressure_mb'],
+            'wind_speed': data['current']['wind_kph']
         }
     return None
 
 class WeatherAPI(Resource):
     """Weather API resource class"""
     @token_required()
-    def get(self, location):
-        """Fetch weather for a specific location."""
-        location = location.lower()
+    def get(self, city):
+        """Fetch weather for a specific city."""
+        city = city.strip().lower()
 
-        if location not in LOCATIONS:
+        # Fetch coordinates using the city name
+        coordinates = fetch_coordinates(city)
+        
+        if not coordinates:
             return jsonify({"error": "Location not found."}), 404
-
-        lat, lon = LOCATIONS[location]["lat"], LOCATIONS[location]["lon"]
+        
+        lat, lon = coordinates
         weather_data = fetch_weather(lat, lon)
 
         if weather_data:
             return jsonify({
-                "location": location.replace("-", " ").title(),
+                "location": city.replace("-", " ").title(),
                 "temperature": weather_data["temperature"],
                 "description": weather_data["description"],
                 "humidity": weather_data["humidity"],
-                "pressure": weather_data["pressure"]
+                "pressure": weather_data["pressure"],
+                "wind_speed": weather_data["wind_speed"]
             })
 
         return jsonify({"error": "Unable to fetch weather data."}), 500
 
-# Add the WeatherAPI resource to the API with the dynamic location parameter.
-api.add_resource(WeatherAPI, '/<string:location>')
+# Add the WeatherAPI resource to the API with the dynamic city parameter.
+api.add_resource(WeatherAPI, '/<string:city>')
 
